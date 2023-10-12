dbb43866299ece74be0e4ec9ce2fd3b9449fea23
Ran3022 <139870642+Ran3022@users.noreply.github.com>
2023-10-12 19:49:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+ document.getElementById("button").addEventListener("click", function () {
+        // Get the values from the input fields
+        var name = document.getElementById("name").value;
+        var year = document.getElementById("year").value;
+
+        // Construct the query string
+        var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+        // Update the URL in the h3 element
+        document.getElementById("url").textContent = "URL: https://localhost:8080/" + queryString;
+    });
